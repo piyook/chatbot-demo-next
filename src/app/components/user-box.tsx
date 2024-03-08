9280f8c629f45a5914e10c71a9da4cb28c932612
piyook <piyook@piyook.com>
2024-03-08 17:04:07 +0000

    chore(app): improve styling

--- src/app/components/user-box.tsx
+++ src/app/components/user-box.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import 'bootswatch/dist/solar/bootstrap.min.css';
-import { BiUser } from 'react-icons/bi';
-import '../App.css';
+import React from "react";
+import "bootswatch/dist/solar/bootstrap.min.css";
+import { BiUser } from "react-icons/bi";
 
 function UserBox({
-	userQuestion,
+  userQuestion,
 }: {
-	readonly userQuestion: string;
+  readonly userQuestion: string;
 }): React.JSX.Element {
-	return (
-		<div className="UserBox d-flex flex-column justify-content-end align-items-end">
-			<BiUser className="BotIcon BotIcon--chat mb-4 bg-info text-white" />
-			<p className="UserBox__userOutput bg-light">{userQuestion}</p>
-		</div>
-	);
+  return (
+    <div className="UserBox d-flex flex-column justify-content-end align-items-end">
+      <BiUser className="BotIcon BotIcon--chat mb-4 bg-info text-white" />
+      <p className="UserBox__userOutput bg-light">{userQuestion}</p>
+    </div>
+  );
 }
 
 export default UserBox;
