e55d3a5457aab9327ba59c5b7b18002a81a7a0f3
piyook <piyook@piyook.com>
2024-12-19 10:19:53 +0000

    chore(): update to next15 and react19

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    swcMinify: true,
     webpack: (config, { webpack }) => {
-      config.experiments = { ...config.experiments, topLevelAwait: true };
-      config.externals["node:fs"] = "commonjs node:fs";
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-    };
-      config.plugins.push(
+        config.experiments = { ...config.experiments, topLevelAwait: true };
+        config.externals['node:fs'] = 'commonjs node:fs';
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+        };
+        config.plugins.push(
+            new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+                resource.request = resource.request.replace(/^node:/, '');
+            }),
+        );
 
-        new webpack.NormalModuleReplacementPlugin(
-          /^node:/,
-          (resource) => {
-            resource.request = resource.request.replace(/^node:/, '');
-          },
-        ),
-      );
-  
-      return config;
-    }
-  }
+        return config;
+    },
+};
 
 export default nextConfig;
